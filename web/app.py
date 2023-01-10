e18dd80521714e676820686f81a4c4d43da9b691
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-01-10 19:31:37 +0530

    push

--- web/app.py
+++ web/app.py
@@ -38,4 +38,4 @@ def add_create_task():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
